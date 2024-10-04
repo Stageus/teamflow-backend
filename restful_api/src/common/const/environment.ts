5140468dfe2f443e4ea53c594ebc14d974243090
최민서 <choeminseo@choeminseoui-MacBookAir.local>
2024-10-04 16:01:21 +0900

    google and pg env setting

--- restful_api/src/common/const/environment.ts
+++ restful_api/src/common/const/environment.ts
@@ -1,1 +1,12 @@
-export const port = process.env.PORT
+export const serverPort = process.env.PORT!
+
+export const googleClientId = process.env.GOOGLE_CLIENT_ID!
+export const googleClientSecret = process.env.GOOGLE_CLIENT_SECRET!
+export const googleRedirectUrl = process.env.GOOGLE_REDIRECT_URL!
+
+export const pgID = process.env.PG_ID!
+export const pgPW = process.env.PG_PW!
+export const pgHost = process.env.PG_HOST!
+export const pgPort = process.env.PG_PORT!
+export const pgDatabase = process.env.PG_DATABASE!
+export const pgPoolMax = 5!
