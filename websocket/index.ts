f9c234a2037b4f5f77774ab961863fcea2c823ca
최민서 <choeminseo@choeminseoui-MacBookAir.local>
2024-11-01 15:12:42 +0900

    jwt 검증하는 middleware 에러 처리 이외 구조 완성

--- websocket/index.ts
+++ websocket/index.ts
@@ -1,18 +1,79 @@
 import 'dotenv/config'
-import express from 'express'
+import express, { Request, Response, NextFunction } from 'express'
 import http from 'http'
 import { Server } from 'socket.io'
 import pool from './common/database/postgresql'
 import { privateType } from './common/const/ch_type'
+import jwt from 'jsonwebtoken'
+import { jwtAccessSecretKey, jwtRefreshSecretKey } from './common/const/environment'
 
 const app = express()
 const server = http.createServer(app)
 
 const io = new Server(server, {
     cors: {
-        origin: '*',
-        credentials: true
+        origin: '*'
     }
+})
+
+io.use((socket, next) => {
+    console.log(socket)
+    const accessTokenHeader = socket.handshake.headers.accesstoken?.toString()
+    const refreshTokenHeader = socket.handshake.headers.refreshtoken?.toString()
+
+    if (!accessTokenHeader) {
+        return next(new Error('no token provided'))
+    }
+
+    if (!refreshTokenHeader) {
+        return next(new Error('need to login'))
+    }
+
+    let accessTokenValid = false
+
+    jwt.verify(accessTokenHeader, jwtAccessSecretKey, (err: any, decoded: any) => {
+        if (err) {
+            accessTokenValid = false
+        } else {
+            accessTokenValid = true
+        }
+    })
+
+    let accessTokenDecoded
+
+    if (!accessTokenValid) {
+        const refreshTokenDecoded = jwt.verify(
+            refreshTokenHeader,
+            jwtRefreshSecretKey
+        ) as jwt.JwtPayload
+
+        const accessToken = jwt.sign(
+            {
+                userIdx : refreshTokenDecoded.userIdx
+            },
+            jwtAccessSecretKey,
+            {
+                issuer: "choiminseo",
+                expiresIn: "30m"
+            }
+        )
+
+        accessTokenDecoded = jwt.verify(
+            accessToken,
+            jwtAccessSecretKey
+        ) as jwt.JwtPayload
+
+        socket.data.accessToken = accessToken
+    } else {
+        accessTokenDecoded = jwt.verify(
+            accessTokenHeader,
+            jwtAccessSecretKey
+        ) as jwt.JwtPayload
+    }
+
+    socket.data.userIdx = accessTokenDecoded.userIdx
+
+    next()
 })
 
 io.on('connection', (socket) => {
@@ -28,7 +89,7 @@ io.on('connection', (socket) => {
     })
 
     socket.on('join_channel', async (channelIdx : number) => {
-
+        socket.join(channelIdx.toString())
     })
 })
 
