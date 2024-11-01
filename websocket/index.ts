9691fa54357656cb97afe9e41e11a0b2506a35ef
choiminseo <161548105+chlalstj1999@users.noreply.github.com>
2024-11-01 15:34:51 +0900

    Develop (#32)
    * interface 정의 추가
    
    * 초대 알람 목록 가져오기
    
    * 초대 수락 및 거절 api 완성'
    
    * 변수명 변경
    
    * controller  conflict 해결
    
    * repo 완성
    
    * thread list 가져오기 api 완성
    
    * thread table content 추가
    
    * send invitation mail utils update
    
    * teamspace 404 error 추가
    
    * 작성된 thread가 없을 경우 not found error 발생 추가
    
    * team-space router service logic and repo logic update
    
    * channel router service logic and repository logic update
    
    * teamspaces router service logic and repo logic update
    
    * notifications router service logic and repo logic update
    
    * notifications router service logic and repo logic update
    
    * threads repo and service logic update
    
    * 안쓰는 import값들 제거
    
    * teamspace api 주석 추가
    
    * user api 주석 추가
    
    * thread의 service로직에 주석 추가
    
    * notification의 service로직에 주석 추가
    
    * invitation의 service로직에 주석 추가
    
    * channel의 service로직에 주석 추가
    
    * 채널 나가기 service logic 완성
    
    * channel 나가기 api 완성
    
    * teamspace 나가기 api 완성
    
    * teamspace 가입 시 기본 공개 채널과 비공개 채널에 join
    
    * teamspace 가입 시 비공개 채널을 제외하고 가입
    
    * generateToken user_idx -> userIdx
    
    * jwt 검증하는 middleware 에러 처리 이외 구조 완성
    
    * check verify token accesstoken이 유효하지 않은 경우에만 refresh token 확인
    
    * jwt 유효성 검사 query 대신 header로 변경
    
    ---------
    
    Co-authored-by: 최민서 <choeminseo@choeminseoui-MacBookAir.local>

--- websocket/index.ts
+++ websocket/index.ts
@@ -3,27 +3,92 @@ import express from 'express'
 import http from 'http'
 import { Server } from 'socket.io'
 import pool from './common/database/postgresql'
+import { privateType } from './common/const/ch_type'
+import jwt from 'jsonwebtoken'
+import { jwtAccessSecretKey, jwtRefreshSecretKey } from './common/const/environment'
 
 const app = express()
 const server = http.createServer(app)
 
 const io = new Server(server, {
     cors: {
-        origin: '*',
-        credentials: true
+        origin: '*'
     }
+})
+
+io.use((socket, next) => {
+    const accessTokenHeader = socket.handshake.headers.accesstoken?.toString()
+    const refreshTokenHeader = socket.handshake.headers.refreshtoken?.toString()
+
+    if (!accessTokenHeader) {
+        return next(new Error('no token provided'))
+    }
+
+    let accessTokenValid = false
+
+    jwt.verify(accessTokenHeader, jwtAccessSecretKey, (err: any, decoded: any) => {
+        if (err) {
+            accessTokenValid = false
+        } else {
+            accessTokenValid = true
+        }
+    })
+
+    let accessTokenDecoded
+
+    if (!accessTokenValid) {
+        if (!refreshTokenHeader) {
+            return next(new Error('need to login'))
+        }
+
+        const refreshTokenDecoded = jwt.verify(
+            refreshTokenHeader,
+            jwtRefreshSecretKey
+        ) as jwt.JwtPayload
+
+        const accessToken = jwt.sign(
+            {
+                userIdx : refreshTokenDecoded.userIdx
+            },
+            jwtAccessSecretKey,
+            {
+                issuer: "choiminseo",
+                expiresIn: "30m"
+            }
+        )
+
+        accessTokenDecoded = jwt.verify(
+            accessToken,
+            jwtAccessSecretKey
+        ) as jwt.JwtPayload
+
+        socket.data.accessToken = accessToken
+    } else {
+        accessTokenDecoded = jwt.verify(
+            accessTokenHeader,
+            jwtAccessSecretKey
+        ) as jwt.JwtPayload
+    }
+
+    socket.data.userIdx = accessTokenDecoded.userIdx
+
+    next()
 })
 
 io.on('connection', (socket) => {
     socket.on('join_teamspace', async (teamspaceIdx : number) => {
         const channelIdxQueryResult = await pool.query(
-            `SELECT ch_idx FROM team_flow_management.channel WHERE ts_idx=$1`,
-            [teamspaceIdx]
+            `SELECT ch_idx FROM team_flow_management.channel WHERE ts_idx=$1 AND ch_type_idx <> $2`,
+            [teamspaceIdx, privateType]
         )
 
         for (let i = 0; i < channelIdxQueryResult.rows.length; i++) {
             socket.join(channelIdxQueryResult.rows[i])
         }
+    })
+
+    socket.on('join_channel', async (channelIdx : number) => {
+        socket.join(channelIdx.toString())
     })
 })
 
